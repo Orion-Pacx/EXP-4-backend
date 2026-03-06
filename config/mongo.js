887f7c029fe1cc1687c57f25d8eefc6b26e32fcf
Orion-Pacx <adityasinghdragon6@gmail.com>
2026-03-06 21:24:04 +0530

    Update mongo.js

--- config/mongo.js
+++ config/mongo.js
@@ -1,1 +1,18 @@
+import mongoose from "mongoose"
 
+export default async function connectMongo() {
+
+  try {
+
+    await mongoose.connect(process.env.MONGO_URI)
+
+    console.log("MongoDB connected")
+
+  } catch (err) {
+
+    console.log("MongoDB connection failed")
+    console.error(err)
+
+  }
+
+}
