1af06c1e4baef686866b42f0fd1366616efdf6e1
Orion-Pacx <adityasinghdragon6@gmail.com>
2026-03-06 21:26:51 +0530

    Update routes.js

--- 2.1.3/routes.js
+++ 2.1.3/routes.js
@@ -1,1 +1,22 @@
+import express from "express"
+import {
+  createProduct,
+  getProducts,
+  updateStock,
+  addReview,
+  topProducts
+} from "./controller.js"
 
+const router = express.Router()
+
+router.post("/products", createProduct)
+
+router.get("/products", getProducts)
+
+router.patch("/stock/:sku", updateStock)
+
+router.post("/review/:productId", addReview)
+
+router.get("/aggregation/top-products", topProducts)
+
+export default router
