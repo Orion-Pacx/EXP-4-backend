b71cbae1df2a7837235ee8bec9ddc0d19955a9d2
Orion-Pacx <adityasinghdragon6@gmail.com>
2026-03-06 21:24:45 +0530

    Update route.js

--- 1.4.3/route.js
+++ 1.4.3/route.js
@@ -1,1 +1,19 @@
+import express from "express"
+import {
+  getSeats,
+  lockSeat,
+  confirmSeat,
+  releaseSeat
+} from "./controller.js"
 
+const router = express.Router()
+
+router.get("/seats", getSeats)
+
+router.post("/lock-seat", lockSeat)
+
+router.post("/confirm-seat", confirmSeat)
+
+router.post("/release-seat", releaseSeat)
+
+export default router
