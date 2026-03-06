69ae9b31cd2c6056075aa5115437b489ae1327de
Orion-Pacx <adityasinghdragon6@gmail.com>
2026-03-06 21:26:19 +0530

    Update Product.js

--- 2.1.3/Product.js
+++ 2.1.3/Product.js
@@ -1,1 +1,49 @@
+import mongoose from "mongoose"
 
+const reviewSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+  },
+  comment: String
+}, { _id: false })
+
+const variantSchema = new mongoose.Schema({
+  sku: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true
+  },
+  color: String,
+  price: Number,
+  stock: Number
+}, { _id: false })
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    index: true
+  },
+  category: {
+    type: String,
+    index: true
+  },
+  variants: [variantSchema],
+  reviews: [reviewSchema],
+  avgRating: {
+    type: Number,
+    default: 0
+  }
+})
+
+productSchema.index({ name: 1, category: 1 })
+
+export default mongoose.model("Product", productSchema)
