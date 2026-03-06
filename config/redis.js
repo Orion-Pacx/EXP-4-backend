4f8027fee35ff1054bbc1c40504141828edab063
Orion-Pacx <adityasinghdragon6@gmail.com>
2026-03-06 21:23:40 +0530

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -1,1 +1,29 @@
+import { createClient } from "redis"
 
+let redisClient
+
+export default async function connectRedis() {
+
+  try {
+
+    redisClient = createClient({
+      url: process.env.REDIS_URL
+    })
+
+    redisClient.on("error", (err) => {
+      console.log("Redis error:", err)
+    })
+
+    await redisClient.connect()
+
+    console.log("Redis connected")
+
+  } catch (err) {
+
+    console.log("Redis connection failed")
+
+  }
+
+}
+
+export { redisClient }
